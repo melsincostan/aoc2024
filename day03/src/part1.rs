9e99873249effec9d27a79aeb8d3263c64d556b3
Mélusine <melusine.hermier@proton.me>
2024-12-03 12:26:10 +0100

    prepare for day 3

---
+++ day03/src/part1.rs
@@ -0,0 +1,13 @@
+pub fn solve(path: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use crate::part1::solve;
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(solve("sample.txt"), 161);
+    }
+}
