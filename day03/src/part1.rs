3dd31244c3b430365ee9041c65f049c995b47595
Mélusine <melusine.hermier@proton.me>
2024-12-03 12:50:39 +0100

    solve part 1

--- day03/src/part1.rs
+++ day03/src/part1.rs
@@ -1,5 +1,26 @@
+use std::fs;
+
+use regex::Regex;
+
 pub fn solve(path: &str) -> u32 {
-    0
+    let re = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)").unwrap();
+    let hay = fs::read_to_string(path).unwrap();
+    re.find_iter(&hay).map(|m| m.as_str()).map(parse_mul).sum()
+}
+
+fn parse_mul(mul: &str) -> u32 {
+    let re = Regex::new(r"mul\((?<a>[0-9]{1,3}),(?<b>[0-9]{1,3})\)").unwrap();
+    let cap: Vec<(&str, &str)> = re
+        .captures_iter(mul)
+        .map(|m| {
+            let a = m.name("a").unwrap().as_str();
+            let b = m.name("b").unwrap().as_str();
+            (a, b)
+        })
+        .collect();
+    let a = cap[0].0.parse::<u32>().unwrap();
+    let b = cap[0].1.parse::<u32>().unwrap();
+    a * b
 }
 
 #[cfg(test)]
