56cbd56f200cc9c519e7e5675f215fdcffd087d6
Mélusine <melusine.hermier@proton.me>
2024-12-04 18:49:37 +0100

    prepare for day 5

---
+++ day05/src/part2.rs
@@ -0,0 +1,14 @@
+pub fn solve(path: &str) -> u32 {
+    1
+}
+
+#[cfg(test)]
+mod test {
+    use crate::part2::solve;
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(solve("sample.txt"), 1);
+    }
+}
+
