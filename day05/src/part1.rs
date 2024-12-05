5200ff1c9a82d0a573ff42a967b5131691639b08
Mélusine <melusine.hermier@proton.me>
2024-12-05 08:43:34 +0100

    call correct function

--- day05/src/part1.rs
+++ day05/src/part1.rs
@@ -3,13 +3,14 @@ use std::{collections::HashMap, fs};
 pub fn solve(path: &str) -> u32 {
     let input = fs::read_to_string(path).unwrap();
     let parts: Vec<&str> = input.split("\n\n").collect();
+    let rules = parse_rules(parts[0]);
     assert_eq!(parts.len(), 2);
     0
 }
 
 fn parse_rules(raw: &str) -> HashMap<u32, Vec<u32>> {
     let mut res: HashMap<u32, Vec<u32>> = HashMap::new();
-    raw.split("\n").map(parse_rule).for_each(|r| {
+    raw.split_whitespace().map(parse_rule).for_each(|r| {
         if res.contains_key(&r.0) {
             let mut curr = res.get(&r.0).unwrap().to_vec();
             curr.push(r.1);
@@ -22,7 +23,10 @@ fn parse_rules(raw: &str) -> HashMap<u32, Vec<u32>> {
 }
 
 fn parse_rule(raw: &str) -> (u32, u32) {
-    let spl: Vec<u32> = raw.split("|").map(|l| l.parse::<u32>().unwrap()).collect();
+    let spl: Vec<u32> = raw
+        .split("|")
+        .map(|l| l.trim().parse::<u32>().unwrap())
+        .collect();
     assert_eq!(spl.len(), 2);
     (spl[0], spl[1])
 }
