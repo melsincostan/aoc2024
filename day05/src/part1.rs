7c2822363d0137f0be485f5cbd94c655b01e2391
Mélusine <melusine.hermier@proton.me>
2024-12-05 11:32:14 +0100

    instead of having the list of what must come after for each entry in rules, have what must come before and check if it is afterwards

--- day05/src/part1.rs
+++ day05/src/part1.rs
@@ -6,20 +6,56 @@ use std::{
 pub fn solve(path: &str) -> u32 {
     let input = fs::read_to_string(path).unwrap();
     let parts: Vec<&str> = input.split("\n\n").collect();
-    let rules = parse_rules(parts[0]);
     assert_eq!(parts.len(), 2);
-    0
+    let rules = parse_rules(parts[0]);
+    let updates: Vec<Vec<u32>> = parts[1].split_whitespace().map(parse_update).collect();
+    updates
+        .into_iter()
+        .filter(|u| check_update(&rules, u))
+        .map(middle)
+        .sum()
+}
+
+fn middle(arr: Vec<u32>) -> u32 {
+    assert_eq!(arr.len() % 2, 1); // how would one get the middle of an even list??
+    arr[arr.len().div_ceil(2) - 1]
+}
+
+fn check_update(rules: &HashMap<u32, HashSet<u32>>, update: &Vec<u32>) -> bool {
+    for i in 0..update.len() {
+        let after: HashSet<u32> =
+            HashSet::from_iter(update[i..update.len()].to_owned().into_iter());
+        if rules.contains_key(&update[i]) {
+            let u = after.intersection(rules.get(&update[i]).unwrap());
+            let overlap = u.count();
+            if overlap > 0 {
+                return false;
+            }
+        } else {
+            continue;
+        }
+    }
+    true
+}
+
+fn parse_update(raw: &str) -> Vec<u32> {
+    raw.trim()
+        .split(",")
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect()
 }
 
 fn parse_rules(raw: &str) -> HashMap<u32, HashSet<u32>> {
+    // for each found entry, list what numbers must come before it
     let mut res: HashMap<u32, HashSet<u32>> = HashMap::new();
     raw.split_whitespace().map(parse_rule).for_each(|r| {
-        if res.contains_key(&r.0) {
-            let mut curr = res.get(&r.0).unwrap().to_owned();
-            curr.insert(r.1);
-            res.insert(r.0, curr);
+        if res.contains_key(&r.1) {
+            // if a comes after b, then b must come after a
+            let mut curr = res.get(&r.1).unwrap().to_owned();
+            curr.insert(r.0);
+            res.insert(r.1, curr);
         } else {
-            res.insert(r.0, HashSet::from_iter(vec![r.1].into_iter()));
+            res.insert(r.1, HashSet::from_iter(vec![r.0].into_iter()));
         }
     });
     res
@@ -31,7 +67,7 @@ fn parse_rule(raw: &str) -> (u32, u32) {
         .map(|l| l.trim().parse::<u32>().unwrap())
         .collect();
     assert_eq!(spl.len(), 2);
-    (spl[0], spl[1])
+    (spl[0], spl[1]) // a must come before b
 }
 
 #[cfg(test)]
