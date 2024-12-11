0442a49508f53430b6db3efe8176da1381e4f512
Mélusine <melusine.hermier@proton.me>
2024-12-11 20:16:31 +0100

    solve part 2 with memoization

--- day11/src/part2.rs
+++ day11/src/part2.rs
@@ -1,13 +1,37 @@
-pub fn solve(path: &str) -> u32 {
-    1
+use std::{collections::HashMap, fs};
+
+pub fn solve(path: &str) -> u64 {
+    let mut cache: HashMap<(u64, u64), u64> = HashMap::new();
+    fs::read_to_string(path)
+        .unwrap()
+        .split_whitespace()
+        .map(|i| stone_amount(&mut cache, i.parse::<u64>().unwrap(), 75))
+        .sum()
 }
 
-#[cfg(test)]
-mod test {
-    use crate::part2::solve;
-
-    #[test]
-    fn test_solve() {
-        assert_eq!(solve("sample.txt"), 1);
+fn stone_amount(cache: &mut HashMap<(u64, u64), u64>, value: u64, iter: u64) -> u64 {
+    if iter == 0 {
+        1
+    } else if cache.contains_key(&(value, iter)) {
+        cache.get(&(value, iter)).unwrap().to_owned()
+    } else if value == 0 {
+        let res = stone_amount(cache, 1, iter - 1);
+        cache.insert((value, iter), res);
+        res
+    } else if value.to_string().len() % 2 == 0 {
+        let s = split(value.to_string());
+        let res = stone_amount(cache, s.0, iter - 1) + stone_amount(cache, s.1, iter - 1);
+        cache.insert((value, iter), res);
+        res
+    } else {
+        let res = stone_amount(cache, value * 2024, iter - 1);
+        cache.insert((value, iter), res);
+        res
     }
+}
+
+fn split(value: String) -> (u64, u64) {
+    let a = value[0..value.len() / 2].parse::<u64>().unwrap();
+    let b = value[value.len() / 2..value.len()].parse::<u64>().unwrap();
+    (a, b)
 }
