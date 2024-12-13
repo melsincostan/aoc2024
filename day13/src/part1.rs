f19de864bda9354ade39ea536dd19507d0e62d41
Mélusine <melusine.hermier@proton.me>
2024-12-13 20:15:25 +0100

    the parts weren't swapped

--- day13/src/part1.rs
+++ day13/src/part1.rs
@@ -12,11 +12,11 @@ fn result(ax: i64, ay: i64, bx: i64, by: i64, px: i64, py: i64) -> Option<(i64, 
     } else {
         let pa = (px * by - py * bx) / (ax * by - ay * bx); // get the amount of presses on a using some simplified form of the two equations
         let btop = pa * ax - px;
-        if bx == 0 || btop % bx == 0 {
+        if bx == 0 || btop % bx != 0 {
             // again, either division by 0 or non-integer result. Not super interesting...
             None
         } else {
-            let pb = (pa * ax - px) / bx; // now that we have that, we can use the x computation to get the amounts of presses on b
+            let pb = -(pa * ax - px) / bx; // now that we have that, we can use the x computation to get the amounts of presses on b
             Some((pa, pb))
         }
     }
@@ -24,10 +24,16 @@ fn result(ax: i64, ay: i64, bx: i64, by: i64, px: i64, py: i64) -> Option<(i64, 
 
 #[cfg(test)]
 mod test {
-    use crate::part1::solve;
+    use crate::{part1::solve, part2::result};
 
     #[test]
     fn test_solve() {
         assert_eq!(solve("sample.txt"), 0);
+    }
+
+    #[test]
+    fn test_result() {
+        assert_eq!(result(94, 34, 22, 67, 8400, 5400), Some((80, 40)));
+        assert_eq!(result(26, 66, 67, 21, 12748, 12176), None);
     }
 }
