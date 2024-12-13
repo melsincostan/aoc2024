3d4dbedc0d67a6727745090da62b2a3272237974
Mélusine <melusine.hermier@proton.me>
2024-12-13 18:47:51 +0100

    prepare for day 13

---
+++ day13/src/main.rs
@@ -0,0 +1,7 @@
+pub mod part1;
+pub mod part2;
+
+fn main() {
+    println!("part 1: {}", part1::solve("input.txt"));
+    println!("part 2: {}", part2::solve("input.txt"));
+}
---
+++ day13/src/part2.rs
@@ -0,0 +1,13 @@
+pub fn solve(path: &str) -> u64 {
+    1
+}
+
+#[cfg(test)]
+mod test {
+    use crate::part2::solve;
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(solve("sample.txt"), 1);
+    }
+}
