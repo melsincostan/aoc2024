741d4c75b6ab3a7fbaa6ad4b8c3a3fcf2d020cb9
Mélusine <melusine.hermier@proton.me>
2024-12-14 09:54:05 +0100

    actually prepare for day 14

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,3 +1,7 @@
+pub mod part1;
+pub mod part2;
+
 fn main() {
-    println!("Hello, world!");
+    println!("part 1: {}", part1::solve("input.txt"));
+    println!("part 2: {}", part2::solve("input.txt"));
 }
